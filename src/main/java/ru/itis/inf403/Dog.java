878f3817b94d3b2fa5222ae91515187cb94c42e6
kgasymly <kanangasymly3@gmail.com>
2024-12-11 09:44:21 +0300

    kgasymly

--- src/main/java/ru/itis/inf403/Dog.java
+++ src/main/java/ru/itis/inf403/Dog.java
@@ -1,14 +1,69 @@
 package ru.itis.inf403;
 
 
-public class Dog extends Animal{
+public class Dog extends Animal {
     public String name;
-    public String eyesColor;
+    public String coatColor;
     public String poroda;
-    public static void bark() {
-        System.out.println("Gav gav");
+
+    public Dog() {
     }
+
+    public Dog(String name, String coatColor, String poroda) {
+        this.name = name;
+        this.coatColor = coatColor;
+        this.poroda = poroda;
+    }
+
+    public void bark() {
+        System.out.println(name +": 'Гав гав'");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCoatColor() {
+        return coatColor;
+    }
+
+    public String getPoroda() {
+        return poroda;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEyesColor(String eyesColor) {
+        this.coatColor = coatColor;
+    }
+
+    public void setPoroda(String poroda) {
+        this.poroda = poroda;
+    }
+
     public void eat() {
-        System.out.println("Dog is eating");
+        System.out.println(name + " кушает");
     }
+
+    public void sleep() {
+        System.out.println(name + " спит");
+    }
+
+    public void coatColor() {
+        System.out.println("у " + name + " " + coatColor + " цвет шерсти");
+    }
+
+    public void poroda() {
+        System.out.println("порода " + name + " : " + poroda);
+    }
+
+    public static Dog[] createDogs() {
+        Dog dog1 = new Dog("Шарик", "рыже-золотистый","Шпиц");
+        Dog dog2 = new Dog("Тузик", "черный", "Бульдог");
+        Dog[] arr1 = {dog1,dog2};
+        return arr1;
+    }
+
 }
--- src/main/java/ru/itis/inf403/Main.java
+++ src/main/java/ru/itis/inf403/Main.java
@@ -1,5 +1,6 @@
 package ru.itis.inf403;
 
+
 public class Main {
     public static void main(String[] args) {
         Animal animal = new Animal();
@@ -7,9 +8,15 @@ public class Main {
         Cat cat = new Cat();
         Bird bird = new Bird();
         Eagle eagle = new Eagle();
-        Hawk hawk = new Hawk();
-        bird.eat();
-        eagle.fly();
+        Dog[] arr1 = Dog.createDogs();
+        Dog s1 = arr1[0];
+        Dog s2 = arr1[1];
+
+        s2.coatColor();
+        s2.poroda();
+        s2.eat();
+        s2.sleep();
+        s2.bark();
 
     }
 }
